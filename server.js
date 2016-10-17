bced2eba5c8f111ab64bd5d98b0f1aa5fc768ee7
haas <haasdevelopment@gmail.com>
2016-10-16 20:42:40 -0500

    return formatted string

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const koa = require('koa');
 const app = koa();
 
 app.use(function *(){
-	this.body = 'Hello World';
+	const reqString = this.request.url.substr(1);
+	const formattedString = reqString.replace(/%20/g, ' ');
+	this.body = JSON.stringify({formattedString});
 });
 
 app.listen(3000);
