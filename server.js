82d48c4f8b571aa7c35dd03b30e9fd75ffefa6fb
haas <haasdevelopment@gmail.com>
2016-10-16 21:10:39 -0500

    convert to unix and standard date
    still need to account for unix date passed in

--- server.js
+++ server.js
@@ -4,8 +4,13 @@ const app = koa();
 
 app.use(function *(){
 	const reqString = this.request.url.substr(1);
-	const formattedString = reqString.replace(/%20/g, ' ');
-	this.body = JSON.stringify({formattedString});
+	if (/%20/.test(reqString)) {
+		const formattedString = reqString.replace(/%20/g, ' ');
+	}
+	const date = new Date(formattedString || reqString);
+	const unix = date.getTime();
+	const standard = date.toDateString();
+	this.body = JSON.stringify({unix, standard});
 });
 
 app.listen(3000);
